5c26485e6ce73eea98b7cdb4f662c87331d2685b
freddy <freddycnash18@hotmail.com>
2017-05-24 20:27:55 +0100

    Added Comment Controller

---
+++ 5 Stars Reviews/Controllers/CommentController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _5_Stars_Reviews.Models;
+
+namespace _5_Stars_Reviews.Controllers
+{
+    public class CommentController : Controller
+    {
+        private CommentContext db = new CommentContext();
+
+        // GET: Comment
+        public ActionResult Index()
+        {
+            var comments = db.Comments.Include(c => c.Film);
+            return View(comments.ToList());
+        }
+
+        // GET: Comment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comment/Create
+        public ActionResult Create()
+        {
+            ViewBag.FilmId = new SelectList(db.Films, "FilmId", "FilmName");
+            return View();
+        }
+
+        // POST: Comment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CommentId,Comments,FilmId")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FilmId = new SelectList(db.Films, "FilmId", "FilmName", comment.FilmId);
+            return View(comment);
+        }
+
+        // GET: Comment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FilmId = new SelectList(db.Films, "FilmId", "FilmName", comment.FilmId);
+            return View(comment);
+        }
+
+        // POST: Comment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CommentId,Comments,FilmId")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FilmId = new SelectList(db.Films, "FilmId", "FilmName", comment.FilmId);
+            return View(comment);
+        }
+
+        // GET: Comment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
